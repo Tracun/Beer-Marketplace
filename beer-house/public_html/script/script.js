c7af4787b43e7ad418d69f11ac8eec99c15cb396
Lucas Lacerda <lucaaslb@hotmail.com>
2018-03-24 18:21:13 -0300

    Netbean Project: Index, register, login
    Realizado layout do index, adicionado a pagina de registro, e pagina de login.

---
+++ beer-house/public_html/script/script.js
@@ -0,0 +1,93 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+function FormatTel(campo, teclapres) {
+    var tecla = teclapres.keyCode;
+    var vr = new String(campo.value);
+    vr = vr.replace("(", "");
+    vr = vr.replace(")", "");
+    vr = vr.replace("-", "");
+    tam = vr.length + 1;
+    if (tecla != 14)
+    {
+        if (tam == 1)
+            campo.value = '(' + vr.substr(0, 1);
+        if (tam == 2)
+            campo.value = '(' + vr.substr(0, 1) + vr.substr(1, 2) + ')';
+        if (tam == 3)
+            campo.value = '(' + vr.substr(0, 1) + vr.substr(1, 2) + ')' + ' ' +
+                    vr.substr(2, 11);
+    }
+}
+
+
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('endereco').value = " ";
+            document.getElementById('bairro').value = " ";
+            document.getElementById('cidade').value = " ";
+            document.getElementById('uf').value = " ";
+
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=cep_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+}
+;
+
+function cep_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        document.getElementById('endereco').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+    } else {
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+    document.getElementById('ibge').value = ("");
+}
