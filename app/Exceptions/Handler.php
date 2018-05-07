3c291796be5c2893ed362cacd9b0532ec638602d
Lucas Lacerda <lucaaslb@hotmail.com>
2018-05-07 20:03:59 -0300

    404 page not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,8 +44,33 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
+    public function render($request, Exception $exception) {
+        // return parent::render($request, $exception);
+
+      if ($this->isHttpException($exception)) {
+        switch ($exception->getStatusCode()) {
+
+           // not authorized
+           // case '403':
+           //     return \Response::view('errors.403',array(),403);
+           //     break;
+
+           // not found
+           case '404':
+               return \Response::view('error.404');
+               break;
+
+           // internal error
+           // case '500':
+           //     return \Response::view('errors.500',array(),500);
+           //     break;
+
+           default:
+               return $this->renderHttpException($exception);
+               break;
+       }
+   } else {
+       return parent::render($request, $exception);
+   }
     }
 }
